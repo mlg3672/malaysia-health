1b523914eaaaaec0380c3ce37a4e87fa9d10d66b
mlg3672 <michele.goe@gmail.com>
2015-05-21 17:39:24 +0800

    use lxml to parse html

--- getpage.py
+++ getpage.py
@@ -1,19 +1,23 @@
+from lxml import etree, html
+from bs4 import BeautifulSoup
+
 def getpages(url,url2,day,time):
     '''
+    This function gets a HTML table from url
     url and url2 are strings of base webaddress
     day is a list of strings that is between url and url2
     time is a list of strings that appends to url2
     '''
-    from bs4 import BeautifulSoup
+    
     
     for i in day:
         for j in time:
             nurl = url + j + url2 + i
-            soup = BeautifulSoup(nurl)
+            htmltree = html.parse(nurl)
             #nfile = readHTMLtable(nurl)
             filename = str(i)+'aqi'+str(j)+'.txt'
             file = open("Documents/R-Projects/malaysia-health/data/"+filename, "w")
-            file.write(str(soup))
+            file.write(str(etree.tostring(htmltree, pretty_print=True)))
             file.close()
 
 #test function
